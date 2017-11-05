a9da8123af478e2edbb0b94558cd8b7f9798c293
ryan <ryan.auger@m2mdtech.com>
2017-11-05 14:45:31 -0500

    no errors

--- configure.sh
+++ configure.sh
@@ -3,14 +3,14 @@
 
 
 ## Git load submodules 
-echo "Loading Submodules\n"       
+echo "Loading Submodules"       
 git submodule init
 git submodule update
 
 ## Set up pathogen
 if [ ! -d .vim/autoload ]
 then
-    echo "Installing Pathogen\n"       
+    echo "Installing Pathogen"       
     cp -r vim-pathogen/autoload .vim/autoload
 fi
 
@@ -18,36 +18,37 @@ fi
 ## Put the submodules into the bundle
 if [ ! -d .vim/bundle/syntastic ]
 then
-    echo "Installing Syntastic\n"       
+    echo "Installing Syntastic"       
     cp -r syntastic .vim/bundle
 fi 
 if [ ! -d .vim/bundle/vim-tmux-navigator ]
 then
-    echo "Installing Tmux-Navigator\n"       
+    echo "Installing Tmux-Navigator"       
     cp -r vim-tmux-navigator .vim/bundle
 fi 
 if [ ! -d .vim/bundle/nerdcommenter ]
 then
-    echo "Installing NerdCommenter\n"       
+    echo "Installing NerdCommenter"       
     cp -r nerdcommenter .vim/bundle
 fi 
 
 
 ## Create the symlinks
-echo "Creating SymLinks\n"
-if [ 
-if [ ! -L ~/.vim && ! -f ~/.vim ] 
+echo "Creating SymLinks"
+if [ ! -L ~/.vim ] && [ ! -f ~/.vim ] 
 then
     ln -s $PWD/.vim ~/.vim
 else 
     rm -rf ~/.vim
     ln -s $PWD/.vim ~/.vim
 fi
-if [ ! -L ~/.vimrc && ! -f ~/.vimrc ] 
+if [ ! -L ~/.vimrc ] && [ ! -f ~/.vimrc ]
 then
     ln -s $PWD/.vimrc ~/.vimrc
 else 
     rm -rf ~/.vimrc
     ln -s $PWD/.vimrc ~/.vimrc
 fi
-echo "Done\n"
+
+echo "Done"
+
