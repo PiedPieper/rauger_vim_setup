2420da253bae4a228fa370dbf4dcd74315a51d32
ryan <ryan.auger@m2mdtech.com>
2017-11-05 13:15:00 -0500

    Setup complete, now for adding tthings

---
+++ .vimrc
@@ -0,0 +1,17 @@
+execute pathogen#infect()
+syntax enable
+set expandtab
+set tabstop=4
+set runtimepath+=~/.vim_runtime
+set number
+set virtualedit=all
+set shell=/bin/bash\ --rcfile\ ~/.bash_profile\ -i
+set wildmode=longest,list,full
+set wildmenu
+
+
+nmap <S-Enter> O<Esc>
+nmap <CR> o<Esc>
+
+
+colorscheme atom-dark-256
---
+++ configure.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+## Git load submodules 
+git submodule init
+git submodule update
+
+## Set up pathogen
+if [ ! -d .vim/autoload ]
+then
+    cp -r vim-pathogen/autoload .vim/autoload
+fi
+
+
+## Put the submodules into the bundle
+if [ ! -d .vim/bundle/syntastic ]
+then
+    cp -r syntastic .vim/bundle
+fi 
+if [ ! -d .vim/bundle/vim-tmux-navigator ]
+then
+    cp -r vim-tmux-navigator .vim/bundle
+fi 
+if [ ! -d .vim/bundle/nerdcommenter ]
+then
+    cp -r nerdcommenter .vim/bundle
+fi 
+ 
+## Create the symlinks
+if [ ! -L ~/.vim ] 
+then
+    ln -s $PWD/.vim ~/.vim
+fi
+
+if [ ! -L ~/.vimrc ] 
+then
+    ln -s $PWD/.vimrc ~/.vimrc
+fi
+
+       
